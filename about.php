37ace010478df5ec4cf84600fcbe34385a8fc2fb
Arham Mirza <102862412+arham10jul@users.noreply.github.com>
2022-12-10 18:34:27 +0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+// echo"<h1>This is about us page<h1>";
+$name="Ali";
+echo"this is name".$name;
+?>
